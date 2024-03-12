0ccefc92ec5d14da6641aa720f1ea52523d7c6cd
james_macena <james.macena@rebase.com.br>
2024-03-12 16:47:09 -0300

    feat: render 404 when policy is not found

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -2,7 +2,9 @@
 
 class PoliciesController < ApplicationController
   def show
-    policy = Policy.find(params[:id])
+    policy = Policy.find_by(id: params[:id])
+    return render status: :not_found if policy.nil?
+
     full_policy_data = policy.attributes.merge(
       insured: policy.insured.attributes,
       vehicle: policy.vehicle.attributes
--- spec/requests/policies_spec.rb
+++ spec/requests/policies_spec.rb
@@ -18,5 +18,14 @@ RSpec.describe 'Policies', type: :request do
       expect(response).to have_http_status(:ok)
       expect(response.body).to eq(expected_response)
     end
+
+    context 'when policy is not found' do
+      it 'returns not found status' do
+        get '/policies/1'
+
+        expect(response).to have_http_status(:not_found)
+        expect(response.body).to be_blank
+      end
+    end
   end
 end
