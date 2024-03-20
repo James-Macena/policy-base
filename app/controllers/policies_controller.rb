a4c3a285e5910eebad0ba62786ae9a69129e9492
james_macena <james.macena@rebase.com.br>
2024-03-20 10:59:10 -0300

    refactor: use head insted of render when not found

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -3,14 +3,14 @@
 class PoliciesController < ApplicationController
   def index
     policies = Policy.includes(:insured, :vehicle)
-    return render status: :not_found if policies.blank?
+    return head :not_found if policies.blank?
 
     render json: policies, include: [:insured, :vehicle]
   end
 
   def show
     policy = Policy.includes(:insured, :vehicle).find_by(id: params[:id])
-    return render status: :not_found if policy.nil?
+    return head :not_found if policy.nil?
 
     render json: policy, include: [:insured, :vehicle]
   end
